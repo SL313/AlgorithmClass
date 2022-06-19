bd0e9dbc33a1558cf6dc7b76c06d92103139811e
Yebin Lee <50792985+SL313@users.noreply.github.com>
2022-06-19 21:10:59 +0900

    Update 01.c

--- FinalAssignment/01.c
+++ FinalAssignment/01.c
@@ -1,12 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int p[11]={0,1,4,5,7,9,11,13,13,15,16}, result[11], cuts[11];
 
 int memoizedCutRod(int n) {
   int max=0;
-  if (n==0)
-    return 0;
-  if (result[n]!=0)
+  if (result[n]>0)
     return result[n];
   for (int i = 1; i <= n; i++) {
     if(max<p[i]+memoizedCutRod(n-i)){
@@ -20,8 +19,8 @@ int memoizedCutRod(int n) {
 
 int main(void) {
   int num=10;
-  printf("The maximum price is %d\n", memoizedCutRod(num));
   cuts[1]-=2*(num/2);
+  printf("The maximum price is %d\n", memoizedCutRod(num));
   for(int i=10; i>=1; i--){
     if(cuts[i]!=0)
       printf("A rod is cut %d times with length %d\n", cuts[i], i);
